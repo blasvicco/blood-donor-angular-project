b170ad174dff83ce384ffec14fabe91c26717a68
blasvicco@gmail.com <blasvicco@gmail.com>
2020-02-27 01:14:36 +0100

    Using process.env in db config.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+const { env } = process;
 module.exports = {
-	url : 'mongodb://dbuser:3x4mpl3@dbdomain.net:11168/dbname'
-}
+	url: `mongodb://${env['DB_USERNAME']}:${env['DB_PASSWORD']}@${env['DB_HOST']}:${env['DB_PORT']}/${env['DB_NAME']}`,
+};
