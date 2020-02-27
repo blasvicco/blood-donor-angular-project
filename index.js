b15ec31343a4ad1e4fbccf6afa2b0b8bcb1a8eb6
blasvicco@gmail.com <blasvicco@gmail.com>
2020-02-27 02:25:56 +0100

    Adding console log to notify db data.

--- index.js
+++ index.js
@@ -12,26 +12,27 @@ var mongoose		= require('mongoose');
 // configuration ===========================================
 
 //connect to our mongoDB database
-mongoose.connect(db.url); 
+console.log(`Connecting to DB: ${db.url)}`);
+mongoose.connect(db.url);
 
 // set our port
-var port = process.env.PORT || 8080; 
+var port = process.env.PORT || 8080;
 
 // get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json()); 
+// parse application/json
+app.use(bodyParser.json());
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
 server.listen(port);
@@ -42,8 +43,8 @@ var socket = io.listen(server);
 //routes ==================================================
 require('./app/routes')(app, socket, smtp); // configure our routes
 
-// shoutout to the user                     
+// shoutout to the user
 console.log('In your face!!! Listening port ' + port);
 
-// expose app           
-exports = module.exports = app;  
+// expose app
+exports = module.exports = app;
